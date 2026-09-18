0d17bf4631957110c750c061855454e3de3de776
Nadiia Bulmak <nbulmak@gmail.com>
2026-09-18 17:08:40 +0300

    fix injectable error

--- src/feeders/feeders.module.ts
+++ src/feeders/feeders.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { FeedersService } from './feeders.service.js';
 import { FeedersController } from './feeders.controller.js';
 import { PrismaModule } from '../prisma/prisma.module.js';
+import { AuthModule } from '../auth/auth.module.js';
 import { FeederRepository } from './repository/feeders.repository.js';
 import { FeedersGateway } from './feeders.gateway.js';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule],
   controllers: [FeedersController],
   providers: [FeedersService, FeederRepository, FeedersGateway],
 })
